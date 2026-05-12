99051deb0b6cac5bee86c0905fb09e32ef013378
Helloworld20-web <cadion.estrella68@gmail.com>
2026-05-12 14:16:15 +0400

    Add files via upload

---
+++ lsts.py
@@ -0,0 +1,30 @@
+lst = [1,2]
+print(lst)
+
+#Append
+lst.append(4)
+print(lst)
+
+#Extend
+lst.extend([5,6])
+print(lst)
+
+#Insert
+lst.insert(0, 0)
+print(lst)
+
+#Remove
+lst.remove(5)
+
+#Pop
+lst.pop()
+print(lst)
+
+#Sort
+lst.insert(3, -5)
+lst.sort()
+print(lst)
+
+#Reverse
+lst.reverse()
+print(lst)
